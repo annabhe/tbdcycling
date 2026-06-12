47821a964c67a0771d7e903b4960bf26b84b7227
anna <annabhe@umich.edu>
2026-06-11 23:25:53 -0400

    fix date in 06-13 poster name

--- src/content/index.js
+++ src/content/index.js
@@ -29,7 +29,7 @@ export const featuredPosterId = "jun-13-2026";
 export const POSTERS = [
   {
     id: "jun-13-2026",
-    src: "content/posters/2026-06-13.jpg",
+    src: "content/posters/2026-06-13.png",
     title: "CCT-RCP Loop",
     date: "June 13, 2026",
     alt: "",
